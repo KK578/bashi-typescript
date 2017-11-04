b8563aedad65976fb9cd45da447d272670933d90
Kevin Kwan <KevinKwan95@googlemail.com>
2017-11-04 11:09:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bashi
+
+This project has been templated by [generator-kk578](https://github.com/KK578/generator-kk578).
+
+## License
+
+[BSD-2 License](http://opensource.org/licenses/bsd-license.php)
+
+Copyright (c) 2015, Kevin Kwan (KevinKwan@googlemail.com).  
+All rights reserved.
+
+*See `LICENSE.md` for full license text.*
---
+++ grunt/eslint.js
@@ -0,0 +1,17 @@
+module.exports = {
+    options: {
+        format: 'node_modules/eslint-formatter-pretty'
+    },
+    project: {
+        files: [
+            {
+                expand: true,
+                src: [
+                    'gruntfile.js',
+                    'grunt/*.js',
+                    'src/**/*.js'
+                ]
+            }
+        ]
+    }
+};
