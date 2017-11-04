0b6c6038dea6db679aa2e76e21ee63d15e75f41e
Kevin Kwan <KevinKwan95@googlemail.com>
2017-11-04 11:34:54 +0000

    Implement environment variable loading

---
+++ app.js
@@ -0,0 +1,18 @@
+// Load environment variables.
+const environmentLoaded = require('dotenv').config();
+
+if (environmentLoaded.error) {
+    const error = environmentLoaded.error;
+
+    switch (error.code) {
+        case "ENOENT":
+            console.error("No .env file was found.");
+            break;
+
+        default:
+            console.error(error.message);
+            break;
+    }
+
+    process.exit(error.errno);
+}
