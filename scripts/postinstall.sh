5fc5e298c14146e4085ba5895ba7e4bde793b6a1
Kevin Kwan <KevinKwan95@googlemail.com>
2017-11-11 15:23:32 +0000

    #9: Make postinstall script more general

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,10 +4,26 @@ if [ ! -z $npm_config_production ]; then
 fi
 
 PREFIX="[Git Hooks]"
-echo "$PREFIX Copying Git Hooks"
-cp githooks/commit-msg.sh .git/hooks/commit-msg
-cp githooks/post-checkout.sh .git/hooks/post-checkout
-echo "$PREFIX Setting Permissions"
-chmod +x .git/hooks/commit-msg .git/hooks/post-checkout
+echo "$PREFIX Finding Git Hooks"
+HOOKS=$(ls -1 githooks | sed -e 's/\..*$//')
+
+for HOOK in $HOOKS;
+do
+    FILE_SAME=`cmp --silent githooks/$HOOK.sh .git/hooks/$HOOK`
+    FILE_SAME=$?
+
+    if [[ $FILE_SAME -eq 0 ]]; then
+        echo "$PREFIX <$HOOK> Already installed."
+    else
+        echo "$PREFIX <$HOOK> Installing."
+        cp githooks/$HOOK.sh .git/hooks/$HOOK
+    fi
+
+    if [[ ! -x .git/hooks/$HOOK ]]; then
+        echo "$PREFIX <$HOOK> Making executable."
+        chmod +x .git/hooks/$HOOK
+    fi
+done
+
 echo "$PREFIX Done"
 echo
