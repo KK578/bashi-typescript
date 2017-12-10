d66940988fce9993f33d20792dc28312623d0bea
Kevin Kwan <KevinKwan95@googlemail.com>
2017-12-10 18:57:36 +0000

    #36: Move stuff around and restart interface designs

---
+++ src/interface/connection-manager/data/index.ts
@@ -0,0 +1,4 @@
+export { IConnectionBotData } from "./IConnectionBotData";
+export { IConnectionChannelData } from "./IConnectionChannelData";
+export { IConnectionDataManager } from "./IConnectionDataManager";
+export { IConnectionUserData } from "./IConnectionUserData";
---
+++ src/interface/connection-manager/message/index.ts
@@ -0,0 +1,4 @@
+export { IMessage } from "./IMessage";
+export { IMessageManager } from "./IMessageManager";
+export { IMessageReceiver } from "./IMessageReceiver";
+export { IMessageSender } from "./IMessageSender";
